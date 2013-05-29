639d456f0d9b65a7bb7a35069c334c01d112ef81
Alexey Volkov <ark@ark-kun.com>
2013-05-30 03:41:49 +0400

    Fixed all problems with type removal.
    Fixed the last two problems with type removal (at least for the MonoGame
    library) - the library is now fully verifiable after piranha.exe
    remove-code-members-types.
    Nested types have different visibility attributes. It's perfectly fine
    to expose a protected internal type via a protected member.
    You must not remove the "<Module>" class.

--- Ark.Piranha/RemovePrivateTypesProcessor.cs
+++ Ark.Piranha/RemovePrivateTypesProcessor.cs
@@ -5,10 +5,9 @@ using System.Linq;
 
 namespace Ark.Piranha {
     public class RemovePrivateTypesProcessor : CecilProcessor {
-
         public override void ProcessModuleTypes(ModuleDefinition moduleDef, IList<TypeDefinition> typeDefs) {
             foreach (var typeDef in typeDefs.ToList()) {
-                if (!typeDef.IsPublic) {
+                if (!typeDef.IsPublic && typeDef.FullName != "<Module>") {
                     moduleDef.Types.Remove(typeDef);
                 }
             }
@@ -17,7 +16,7 @@ namespace Ark.Piranha {
 
         public override void ProcessNestedTypes(TypeDefinition typeDef, IList<TypeDefinition> typeDefs) {
             foreach (var nestedTypeDef in typeDefs.ToList()) {
-                if (!nestedTypeDef.IsPublic) {
+                if (!(nestedTypeDef.IsNestedPublic || nestedTypeDef.IsNestedFamily || nestedTypeDef.IsNestedFamilyOrAssembly)) {
                     typeDef.NestedTypes.Remove(nestedTypeDef);
                 }
             }
