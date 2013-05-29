cc2cfaa90eeab81e18e4dd071507b5da5bedfd7c
Alexey Volkov <ark@ark-kun.com>
2013-05-29 10:02:20 +0400

    Fixed the program.

--- Piranha/Program.cs
+++ Piranha/Program.cs
@@ -19,13 +19,15 @@ namespace Piranha {
             }
             string inputFileName = args[0];
             //string inputFileName = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            string outputFileBase = Path.GetFileNameWithoutExtension(inputFileName);
+            string inputFileBase = Path.Combine(Path.GetDirectoryName(inputFileName), Path.GetFileNameWithoutExtension(inputFileName));
+            string outputFileBase = inputFileBase + ".skeleton";
             string outputFileName = outputFileBase + ".skeleton.dll";
 
             var inputStream = File.OpenRead(inputFileName);
             var outputStream = File.Create(outputFileName);
 
-            var assemblyDef = AssemblyDefinition.ReadAssembly(inputStream, new ReaderParameters() { ReadSymbols = true });
+            bool useSymbols = File.Exists(inputFileBase + ".pdb") || File.Exists(inputFileBase + ".mdb");
+            var assemblyDef = AssemblyDefinition.ReadAssembly(inputStream, new ReaderParameters() { ReadSymbols = useSymbols });
             //assemblyDef.Name.Name += " (Skeleton)";
 
             DumpAssemblyAndUsageLists(assemblyDef, outputFileBase, 0);
@@ -33,12 +35,14 @@ namespace Piranha {
             var usedConstructors = new HashSet<MethodReference>(MethodReferenceEqualityComparer.Default);
 
             //Step 1: Removing all bodies.
-            new RemoveMethodBodiesProcessor().ProcessAssembly(assemblyDef);
+            var codeRemover = new RemoveMethodBodiesProcessor();
+            codeRemover.ProcessAssembly(assemblyDef);
 
             DumpAssemblyAndUsageLists(assemblyDef, outputFileBase, 1);
 
             //Step 2 and 3: Removing all private members
-            new RemovePrivateMembersProcessor().ProcessAssembly(assemblyDef);
+            var membersRemover = new RemovePrivateMembersProcessor(true) { MethodsToPreserve = codeRemover.UsedConstructors };
+            membersRemover.ProcessAssembly(assemblyDef);
 
             DumpAssemblyAndUsageLists(assemblyDef, outputFileBase, 3);
 
@@ -53,7 +57,7 @@ namespace Piranha {
         static void DumpAssemblyAndUsageLists(AssemblyDefinition assemblyDef, string fileNameBase, int step) {
             var oldName = assemblyDef.Name.Name;
             assemblyDef.Name.Name += " " + step.ToString();
-            assemblyDef.Write(fileNameBase + ".skeleton." + step.ToString() + ".dll");
+            assemblyDef.Write(fileNameBase + "." + step.ToString() + ".dll");
             assemblyDef.Name.Name = oldName;
 
             var usedTypesCollector = new CollectUsedTypesProcessor();
