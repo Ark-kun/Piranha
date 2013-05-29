ebc3c6c1b441c5ad32e44a1376ece7485ab948e6
Alexey Volkov <ark@ark-kun.com>
2013-05-29 10:49:56 +0400

    Updated the assembly attributes.

--- Ark.Cecil/Properties/AssemblyInfo.cs
+++ Ark.Cecil/Properties/AssemblyInfo.cs
@@ -1,36 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Ark.Cecil")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Ark.Cecil library: Extension for the Mono.Cecil library.")]
+[assembly: AssemblyCompany("Flying Ark havens")]
 [assembly: AssemblyProduct("Ark.Cecil")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: AssemblyCopyright("©Flying Ark havens 2013")]
+[assembly: AssemblyVersion("0.1.*")]
 [assembly: Guid("99df93bb-b602-4d17-a897-ceeee827a4b5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCulture("")]
