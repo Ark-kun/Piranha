8f98b800bb4cca25b9231ad96e4c3f0260ab448f
Alexey Volkov <ark@ark-kun.com>
2013-05-29 10:03:48 +0400

    Added the RemoveAllReferencesProcessor class.

---
+++ Ark.Piranha/RemoveAllReferencesProcessor.cs
@@ -0,0 +1,11 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+
+namespace Ark.Cecil {
+    public class RemoveAllReferencesProcessor : CecilProcessor {
+        public override void ProcessAssemblyReferences(ModuleDefinition moduleDef, IList<AssemblyNameReference> assemblyNameRefs) {
+            assemblyNameRefs.Clear();
+            base.ProcessAssemblyReferences(moduleDef, assemblyNameRefs);
+        }
+    }
+}
