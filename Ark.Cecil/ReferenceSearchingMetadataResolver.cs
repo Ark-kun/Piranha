3e5c70dcda098e4d0915fdcd1f58ced2beea48d8
Alexey Volkov <ark@ark-kun.com>
2013-06-05 08:48:59 +0400

    Added ReferenceSearchingMetadataResolver.

---
+++ Ark.Cecil/ReferenceSearchingMetadataResolver.cs
@@ -0,0 +1,25 @@
+﻿using Mono.Cecil;
+
+namespace Ark.Cecil {
+    public class ReferenceSearchingMetadataResolver : MetadataResolver {
+        public ReferenceSearchingMetadataResolver(IAssemblyResolver assemblyResolver)
+            : base(assemblyResolver) {
+        }
+
+        public override TypeDefinition Resolve(TypeReference type) {
+            var result = base.Resolve(type);
+            if (result != null) {
+                return result;
+            }
+
+            foreach (var reference in type.Module.AssemblyReferences) {
+                type.Scope = reference;
+                result = base.Resolve(type);
+                if (result != null) {
+                    return result;
+                }
+            }
+            return null;
+        }
+    }
+}
