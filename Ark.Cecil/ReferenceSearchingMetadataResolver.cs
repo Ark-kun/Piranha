f3e9ce605e4b07d1346f414d5841a14dced3d25e
Alexey Volkov <ark@ark-kun.com>
2013-06-09 16:46:03 +0400

    Fixed ReferenceSearchingMetadataResolver.
    Made ReferenceSearchingMetadataResolver resistant to
    AssemblyResolutionException.

--- Ark.Cecil/ReferenceSearchingMetadataResolver.cs
+++ Ark.Cecil/ReferenceSearchingMetadataResolver.cs
@@ -8,19 +8,31 @@ namespace Ark.Cecil {
         }
 
         public override TypeDefinition Resolve(TypeReference type) {
-            var result = base.Resolve(type);
+            TypeDefinition result = null;
+            try {
+                result = TryResolve(type);
+            } catch (AssemblyResolutionException) { }
             if (result != null) {
                 return result;
             }
             var originalScope = type.Scope;
             foreach (var reference in type.Module.AssemblyReferences) {
                 type.Scope = reference;
-                result = base.Resolve(type);
+                try {
+                    result = TryResolve(type);
+                } catch (AssemblyResolutionException) { }
                 if (result != null) {
                     Trace.WriteLine(string.Format("Successfully forwarded the type {0} from {1} to {2}.", type, originalScope, type.Scope), "ReferenceSearchingMetadataResolver");
                     return result;
                 }
             }
+            return null;
+        }
+
+        public TypeDefinition TryResolve(TypeReference type) {
+            try {
+                return base.Resolve(type);
+            } catch (AssemblyResolutionException) { }
             return null;
         }
     }
