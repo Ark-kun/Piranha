61d23008ba48a8fd7f54f5c0bfd631c007efc55b
Alexey Volkov <ark@ark-kun.com>
2013-06-09 17:37:48 +0400

    Added tracing to RemovePrivateTypesProcessor.

--- Ark.Piranha/RemovePrivateTypesProcessor.cs
+++ Ark.Piranha/RemovePrivateTypesProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using Ark.Cecil;
 using Mono.Cecil;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Ark.Piranha {
@@ -8,6 +9,7 @@ namespace Ark.Piranha {
         protected override void ProcessModuleTypes(ModuleDefinition moduleDef, IList<TypeDefinition> typeDefs) {
             foreach (var typeDef in typeDefs.ToList()) {
                 if (!typeDef.IsPublic && typeDef.FullName != "<Module>") {
+                    Trace.WriteLine(string.Format("Removing type {0}.", typeDef), "RemovePrivateTypes");
                     moduleDef.Types.Remove(typeDef);
                 }
             }
@@ -17,6 +19,7 @@ namespace Ark.Piranha {
         protected override void ProcessNestedTypes(TypeDefinition typeDef, IList<TypeDefinition> typeDefs) {
             foreach (var nestedTypeDef in typeDefs.ToList()) {
                 if (!(nestedTypeDef.IsNestedPublic || nestedTypeDef.IsNestedFamily || nestedTypeDef.IsNestedFamilyOrAssembly)) {
+                    Trace.WriteLine(string.Format("Removing type {0}.", nestedTypeDef), "RemovePrivateTypes");
                     typeDef.NestedTypes.Remove(nestedTypeDef);
                 }
             }
