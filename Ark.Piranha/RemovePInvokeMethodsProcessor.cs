184c29de045ffb3d7c9bbc59de9276d8f2fa8a78
Alexey Volkov <ark@ark-kun.com>
2013-06-09 17:36:29 +0400

    Added tracing to RemovePInvokeMethodsProcessor.

--- Ark.Piranha/RemovePInvokeMethodsProcessor.cs
+++ Ark.Piranha/RemovePInvokeMethodsProcessor.cs
@@ -2,10 +2,15 @@
 using Ark.Linq;
 using Mono.Cecil;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Ark.Piranha {
     public class RemovePInvokeMethodsProcessor : CecilProcessor {
         protected override void ProcessMethods(TypeDefinition typeDef, IList<MethodDefinition> methodDefs) {
+            foreach (var removedMethod in methodDefs.Where(methodDef => methodDef.HasPInvokeInfo)) {
+                Trace.WriteLine(string.Format("Removed P/Invoke method {0}.", removedMethod), "RemovePInvokeMethods");
+            }
             methodDefs.RemoveWhere(methodDef => methodDef.HasPInvokeInfo);
             base.ProcessMethods(typeDef, methodDefs);
         }
