e2712d4125149a5eb7265462cc3716d6e0f1befe
Alexey Volkov <ark@ark-kun.com>
2016-06-09 02:28:22 -0700

    Improved the efficiency of the ICollection<T>.RemoveWhere and ICollection<T>.Exclude extension methods.

--- Ark.Cecil/CollectionExtensions.cs
+++ Ark.Cecil/CollectionExtensions.cs
@@ -10,40 +10,73 @@ namespace Ark.Linq {
             }
         }
 
-        public static void RemoveWhere<T>(this ICollection<T> collection, Func<T, bool> predicate) {
+        public static int RemoveWhere<T>(this ICollection<T> collection, Func<T, bool> predicate) {
+            var hashSet = collection as HashSet<T>;
+            if (hashSet != null) {
+                return hashSet.RemoveWhere(predicate);
+            }
+            var sortedSet = collection as SortedSet<T>;
+            if (hashSet != null) {
+                return hashSet.RemoveWhere(predicate);
+            }
             var list = collection as IList<T>;
             if (list != null) {
-                for (int i = list.Count - 1; i >= 0; --i) {
+                int removedCount = 0;
+                for (int i = 0; i < list.Count; i++) {
                     if (predicate(list[i])) {
-                        list.RemoveAt(i);
+                        removedCount++;
+                    } else if(removedCount > 0) {
+                        list[i - removedCount] = list[i];
                     }
                 }
+                for (int i = 0; i < removedCount; i++) {
+                    list.RemoveAt(list.Count - 1);
+                }
+                return removedCount;
             } else {
                 var itemsToRemove = collection.Where(predicate).ToList();
                 foreach (var item in itemsToRemove) {
                     collection.Remove(item);
                 }
+                return itemsToRemove.Count;
             }
         }
 
         public static List<T> Exclude<T>(this ICollection<T> collection, Func<T, bool> predicate) {
-            var removedItems = new List<T>();
             var list = collection as IList<T>;
             if (list != null) {
-                for (int i = list.Count - 1; i >= 0; --i) {
+                var removedItems = new List<T>();
+                for (int i = 0; i < list.Count; i++) {
                     if (predicate(list[i])) {
                         removedItems.Add(list[i]);
-                        list.RemoveAt(i);
+                    } else if(removedItems.Count > 0) {
+                        list[i - removedItems.Count] = list[i];
                     }
                 }
+                for (int i = 0; i < removedItems.Count; i++) {
+                    list.RemoveAt(list.Count - 1);
+                }
+                return removedItems;
             } else {
                 var itemsToRemove = collection.Where(predicate).ToList();
+                
+                var hashSet = collection as HashSet<T>;
+                if (hashSet != null) {
+                    hashSet.RemoveWhere(predicate);
+                    return itemsToRemove;
+                }
+
+                var sortedSet = collection as SortedSet<T>;
+                if (hashSet != null) {
+                    hashSet.RemoveWhere(predicate);
+                    return itemsToRemove;
+                }
+
                 foreach (var item in itemsToRemove) {
-                    removedItems.Add(item);
                     collection.Remove(item);
                 }
+                return itemsToRemove;
             }
-            return removedItems;
         }
 
         public static void ReversedForEach<TSource>(this IList<TSource> list, Action<TSource> action) {
